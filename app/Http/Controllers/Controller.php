d101d8e3749862cbe0308d5617caa63577789d8d
pgcampana <pgcampana@gmail.com>
2015-12-11 12:54:32 +0100

    quarto salvataggio

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use Cache; use App\Http\Controllers\Controller; 
+class UserController extends Controller 
+{ 
+	/** 
+	 * Show the profile for the given user. 
+	 * 
+	 * @param int $id 
+	 * @return Response 
+	 */ 
+	public function showProfile($id) 
+	{ 
+		$user = Cache::get('user:'.$id');
+				
+		return view('profile', ['user' => $user]); 
+	} 
+} 	 
+			
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+	Route::resource('users', 'UsersController');
+	
+	Route::get('home', 'AccountController@home');
+	
